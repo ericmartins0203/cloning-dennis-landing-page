1b52ef570119a6c354ddbc68cb39e0f75d85793e
ericmartins0203 <estevesmartinseric@gmail.com>
2023-10-07 18:33:47 -0300

    component description and adjusts in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,40 +1,69 @@
-'use client'
-import styles from './style.module.scss'
-import { useState, useEffect } from "react";
-import Nav from './Nav';
-import { AnimatePresence } from 'framer-motion';
+'use client';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import styles from './style.module.scss';
 import { usePathname } from 'next/navigation';
-
+import { AnimatePresence } from 'framer-motion';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import Nav from './Nav';
 
 export default function Header() {
-  const [isActive, setIsActive] = useState(false)
+  const header = useRef(null);
+  const [isActive, setIsActive] = useState(false);
   const pathname = usePathname();
+  const button = useRef(null);
 
   useEffect( () => {
     if(isActive) setIsActive(false)
   }, [pathname])
 
-  return (
-    <div className={styles.header}>
-      <div className={styles.logo}>
-        <div className={styles.copyright}>o</div>
-        <div className={styles.name}></div>
-      </div>
+  useLayoutEffect( () => {
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.to(button.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        onLeave: () => {gsap.to(button.current, {scale: 1, duration: 0.25, ease: "power1.out"})},
+        onEnterBack: () => {gsap.to(button.current, {scale: 0, duration: 0.25, ease: "power1.out"})}
+      }
+    })
+  }, [])
 
-      <div className={styles.buttonContainer}>
-        <div 
-          onClick={() => setIsActive(!isActive)} 
-          className={`${styles.button}`}
-        >
-          <div 
-            className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
-          >
-            <AnimatePresence mode="wait">
-              {isActive && <Nav />}
-            </AnimatePresence>
+  return (
+    <>
+      <div ref={header} className={styles.header}>
+        <div className={styles.logo}>
+          <p className={styles.copyright}>©</p>
+          <div className={styles.name}>
+            <p className={styles.codeBy}>Code by</p>
+            <p className={styles.dennis}>Dennis</p>
+            <p className={styles.snellenberg}>Snellenberg</p>
+          </div>
+        </div>
+        <div className={styles.nav}>
+          <div className={styles.el}>
+            <a>Work</a>
+            <div className={styles.indicator}></div>
+          </div>
+          <div className={styles.el}>
+            <a>About</a>
+            <div className={styles.indicator}></div>
+          </div>
+          <div className={styles.el}>
+            <a>Contact</a>
+            <div className={styles.indicator}></div>
           </div>
         </div>
       </div>
-    </div>
+      <div ref={button} className={styles.headerButtonContainer}>
+        <div onClick={() => {setIsActive(!isActive)}} className={`${styles.button}`}>
+          <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+        </div>
+      </div>
+      <AnimatePresence mode="wait">
+        {isActive && <Nav />}
+      </AnimatePresence>
+    </>
   )
 }
